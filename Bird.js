a89e889314da7f207ab008818ecaff86aeacaf5a
sanskriti01 <80110840+sanskriti01@users.noreply.github.com>
2021-03-29 20:20:38 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,32 @@
+class Bird {
+  constructor(x, y) {
+    var options = {
+       
+        
+        'density':1.5,
+        'friction':1,
+
+        'restitution':0.5,
+
+       // isStatic: true
+    };
+    this.body = Bodies.rectangle(x, y, 50, 50, options);
+    this.width = 50;
+    this.height = 50;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    pos.x = mouseX;
+    pos.y = mouseY;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill("red");
+    rect(0, 0, this.width, this.height);
+    pop();
+  };
+};
